89124d4fb1f818f3bdf85b0b493e9cd64f2250d6
Vishesh-Codes <visheshkarnabcd@gmail.com>
2024-01-21 13:34:44 +0530

    imported make_pipeline

--- Sentiment-Analysis-on-Social-Media-Data/sentiment-analysis.py
+++ Sentiment-Analysis-on-Social-Media-Data/sentiment-analysis.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 from torch.utils.data import DataLoader, TensorDataset
